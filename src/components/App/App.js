9d8de6b63765412d5bdab6d361522253c8e1300a
Sam Flavin <flavi022@d.umn.edu>
2019-07-19 16:37:10 -0500

    checking

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Feedback!</h1>
+          <h4><i>Don't forget it!</i></h4>
+          <h6> We know where you live...</h6>
+        </header>
+
+        <br/>
+      </div>
+    );
+  }
+}
+
+export default App;
